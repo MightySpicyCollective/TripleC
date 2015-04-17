f5c90c6d95e0b1fdd9fa16b109ce5056d500b0a0
gianordoli <gianordoligabriel@yahoo.com.br>
2015-04-17 17:36:20 -0400

    Updated models

--- Application/triplec/app/models/user.rb
+++ Application/triplec/app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
 
 	# To configure a different table name:
 	# self.table_name = "admin_users"
-
+	belongs_to :school
 	has_one :teacher
 	has_one :picture
+	has_many :projects
 
 end
