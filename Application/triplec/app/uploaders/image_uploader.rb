7e04e3f842537c2413fb2a07c40b5a05f1da8fda
gianordoli <gianordoligabriel@yahoo.com.br>
2015-05-04 18:44:28 -0400

    Fixed a few things

--- Application/triplec/app/uploaders/image_uploader.rb
+++ Application/triplec/app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # version :thumb do
@@ -43,9 +43,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
 
-  # version :thumb do
-  #   process :resize_to_fill => [600, 450]
-  # end
+  version :thumb do
+    process :resize_to_fill => [600, 450]
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
